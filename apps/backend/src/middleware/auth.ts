621f8e38e29c6e13161ded091e72598a96b0f213
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-11-17 21:57:14 +0530

    added middleware

---
+++ apps/backend/src/middleware/auth.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config.js";
+
+declare global{
+    namespace Express{
+        interface Request{
+            user?: {
+                id: string,
+                email: string
+            }
+        }
+    }
+}
+
+
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction){
+    try{
+        //get token from headers
+        const token = req.cookies.sessionToken || req.headers.authorization?.replace('Bearer ', '');
+        if(!token){
+            return res.status(400).json({
+                message: "Authentication Required"
+            })
+        }
+        // verify payload
+
+        const payload = jwt.verify(token, JWT_SECRET) as any;
+
+        if(payload.type !== 'session'){
+            return res.status(401).json({
+                message: "Invalid token type"
+            })
+        }
+        //attach user to request
+        req.user = {
+            id : payload.userId,
+            email : payload.email
+        };
+        next();
+    }catch(err){
+        console.log(err);
+        res.status(401).json({
+            message: "Invalid of expired token"
+        })
+    }
+}
