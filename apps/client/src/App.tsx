b1409e22b80e6c346f0ac52826f24d1e759f81c0
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-12-01 22:37:00 +0530

    main

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css'
+import { Landing } from './pages/Landing';
+import SignupPage from './pages/Signup';
+import SigninPage from './pages/Signin';
+import { Dashboard } from './pages/Dashboard';
+// import { Workflow } from './pages/Workflow';
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route element={<Landing />} path='/' />
+      <Route element={<SignupPage />} path='/signup' />
+      <Route element={<SigninPage />} path='/signin' />
+      <Route element={<Dashboard />} path='/dashboard' />
+      {/* <Route element={<Workflow />} path='/workflow' /> */}
+
+    </Routes>
+    </BrowserRouter>
+   
+    </>
+  )
+}
+
+export default App
