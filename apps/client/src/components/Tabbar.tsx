90b934f6e62546c9ff8233ce7da3dd0c0e0473b1
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-12-02 23:51:48 +0530

    components

---
+++ apps/client/src/components/Tabbar.tsx
@@ -0,0 +1,17 @@
+import logo from "../assets/logo.png"
+import { Button } from "./ui/button"
+export function Tabbar(){
+    return<div className="flex justify-normal bg-white fixed w-screen h-14">
+        <div className="ml-2 mt-2 bg-transparent">
+        <img src={logo} alt="logo" width={60} height={60} className="w-[60] h-auto -mt-2 -ml-2"></img>
+        </div>
+        <div className="flex ml-auto">
+              <Button  className="bg-corporateBlue text-white p-4 rounded-md mt-3 mb-3 mr-3 ml-2 hover:bg-corporateBlue font-vietnam ">
+            Workflow Editor
+          </Button>
+
+        </div>
+    </div>
+}
+//bg-gradient-to-r from-indigo-950/75 to-violet-600 
+//bg-gradient-to-r from-red-400/75 to-violet-600
