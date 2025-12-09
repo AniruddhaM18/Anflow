617b7e68dece1342b23f585f9187964e52213539
Aniruddha <aniruddhamaradwar9@gmail.com>
2025-12-09 22:29:21 +0530

    axios

--- apps/client/src/lib/config.ts
+++ apps/client/src/lib/config.ts
@@ -1,3 +1,8 @@
+import axios from "axios";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 export { BACKEND_URL };
+
+// Make axios include cookies on ALL requests
+axios.defaults.withCredentials = true;
